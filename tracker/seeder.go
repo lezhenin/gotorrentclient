a56de13dd9c7b5926a7841497095205bfac11e6d
Yuriy Lezhenin <yuriy.lezhenin@spbpu.com>
2019-03-06 20:11:00 +0300

    add methods for message making and parsing

--- tracker/seeder.go
+++ tracker/seeder.go
@@ -3,23 +3,47 @@ package tracker
 import (
 	"encoding/binary"
 	"fmt"
+	"github.com/lezhenin/gotorrentclient/bitfield"
 	"io"
 	"net"
 )
 
+const blockSize int = 16 * 1024
+const bufferSize = blockSize + 16
+
+type MessageId uint8
+
+const (
+	KeepAlive     MessageId = 255 // has no id
+	Choke         MessageId = 0
+	Unchoke       MessageId = 1
+	Interested    MessageId = 2
+	NotInterested MessageId = 3
+	Have          MessageId = 4
+	Bitfield      MessageId = 5
+	Request       MessageId = 6
+	Piece         MessageId = 7
+	Cancel        MessageId = 8
+)
+
 type Seeder struct {
 	NetAddress net.Addr
-	Complete   bool
+
+	Complete bool
 
 	AmChoking      bool
 	AmInterested   bool
 	PeerChoking    bool
 	PeerInterested bool
 
-	PeerId   []byte
+	PeerBitfield bitfield.Bitfield
+	PeerId       []byte
+
+	MyPeerId []byte
 	InfoHash []byte
 
 	connectionTCP io.ReadWriter
+	buffer        []byte
 }
 
 func NewSeeder(addr string, infoHash []byte, peerId []byte) (seeder *Seeder, err error) {
@@ -36,8 +60,10 @@ func NewSeeder(addr string, infoHash []byte, peerId []byte) (seeder *Seeder, err
 	seeder.AmInterested = false
 	seeder.PeerInterested = false
 
-	seeder.PeerId = peerId
+	seeder.MyPeerId = peerId
 	seeder.InfoHash = infoHash
+
+	seeder.buffer = make([]byte, bufferSize)
 
 	return seeder, nil
 
@@ -54,7 +80,7 @@ func (s *Seeder) SendHandshakeMessage() (err error) {
 
 	fmt.Printf("send to %s\n", s.NetAddress.String())
 
-	message := makeHandshakeMessage(s.InfoHash, s.PeerId)
+	message := makeHandshakeMessage(s.InfoHash, s.MyPeerId)
 	_, err = s.connectionTCP.Write(message)
 	if err != nil {
 		return err
@@ -78,4 +104,141 @@ func makeHandshakeMessage(infoHash []byte, peerId []byte) (message []byte) {
 	binary.BigEndian.PutUint64(message[20:28], 0)
 
 	return message
+}
+
+func makeHavePayload(pieceIndex uint32) (payload []byte) {
+
+	payload = make([]byte, 4)
+	binary.BigEndian.PutUint32(payload, pieceIndex)
+	return payload
+}
+
+func parseHavePayload(payload []byte) (pieceIndex uint32, err error) {
+
+	if len(payload) != 4 {
+		return 0,
+			fmt.Errorf("parse have payload: byte slice has wrong length %d", len(payload))
+	}
+
+	pieceIndex = binary.BigEndian.Uint32(payload)
+	return pieceIndex, nil
+}
+
+func makeRequestPayload(pieceIndex uint32, begin uint32, length uint32) (payload []byte) {
+
+	payload = make([]byte, 12)
+	binary.BigEndian.PutUint32(payload[0:4], pieceIndex)
+	binary.BigEndian.PutUint32(payload[4:8], begin)
+	binary.BigEndian.PutUint32(payload[8:12], length)
+	return payload
+}
+
+func parseRequestPayload(payload []byte) (pieceIndex uint32, begin uint32, length uint32, err error) {
+
+	if len(payload) != 12 {
+		return 0, 0, 0,
+			fmt.Errorf("parse request payload: byte slice has wrong length %d", len(payload))
+	}
+
+	pieceIndex = binary.BigEndian.Uint32(payload[0:4])
+	begin = binary.BigEndian.Uint32(payload[4:8])
+	length = binary.BigEndian.Uint32(payload[8:12])
+
+	return pieceIndex, begin, length, nil
+}
+
+func makePiecePayload(pieceIndex uint32, begin uint32, block []byte) (payload []byte) {
+
+	payload = make([]byte, 8+len(block))
+	binary.BigEndian.PutUint32(payload[0:4], pieceIndex)
+	binary.BigEndian.PutUint32(payload[4:8], begin)
+	copy(payload[8:(8+len(block))], block)
+
+	return payload
+}
+
+func parsePiecePayload(payload []byte) (pieceIndex uint32, begin uint32, block []byte, err error) {
+
+	if len(payload) < 8 {
+		return 0, 0, nil,
+			fmt.Errorf("parse piece payload: byte slice length %d less than 9", len(payload))
+	}
+
+	pieceIndex = binary.BigEndian.Uint32(payload[0:4])
+	begin = binary.BigEndian.Uint32(payload[4:8])
+
+	copy(block, payload[8:])
+
+	return pieceIndex, begin, block, nil
+}
+
+func makeCancelPayload(pieceIndex uint32, begin uint32, length uint32) (payload []byte) {
+
+	payload = make([]byte, 12)
+	binary.BigEndian.PutUint32(payload[0:4], pieceIndex)
+	binary.BigEndian.PutUint32(payload[4:8], begin)
+	binary.BigEndian.PutUint32(payload[8:12], length)
+	return payload
+}
+
+func parseCancelPayload(payload []byte) (pieceIndex uint32, begin uint32, length uint32, err error) {
+
+	if len(payload) != 12 {
+		return 0, 0, 0,
+			fmt.Errorf("parse cancel payload: byte slice has wrong length %d", len(payload))
+	}
+
+	pieceIndex = binary.BigEndian.Uint32(payload[0:4])
+	begin = binary.BigEndian.Uint32(payload[4:8])
+	length = binary.BigEndian.Uint32(payload[8:12])
+
+	return pieceIndex, begin, length, nil
+}
+
+func makeMessage(id MessageId, payload []byte) (message []byte) {
+
+	if id == KeepAlive {
+		message = make([]byte, 1)
+		message[0] = 0
+		return message
+	}
+
+	length := 1 + len(payload)
+	message = make([]byte, length+1)
+	message[0] = byte(length)
+	message[1] = byte(id)
+	copy(message[2:length], payload)
+
+	return message
+}
+
+func parseMessage(message []byte) (id MessageId, payload []byte, err error) {
+
+	if len(message) == 0 {
+		return 0, nil,
+			fmt.Errorf("parse message: byte slice is empty")
+	}
+
+	length := int(message[0])
+
+	if length+1 != len(message) {
+		return 0, nil,
+			fmt.Errorf("parse message: byte slice length and message length are not equal")
+	}
+
+	if length == 0 {
+		return KeepAlive, nil, nil
+	}
+
+	id = MessageId(message[1])
+
+	if id > Cancel && id != KeepAlive {
+		return 0, nil,
+			fmt.Errorf("parse message: message has unsupported id %d", id)
+	}
+
+	payload = make([]byte, length-1)
+	copy(payload, message[2:])
+
+	return id, payload, nil
 }
