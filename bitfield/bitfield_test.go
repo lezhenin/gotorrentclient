7f5bf8fd46da4d73550f766e6e3769666c75aa91
Yuriy Lezhenin <yuriy.lezhenin@spbpu.com>
2019-03-18 12:46:03 +0300

    add some tests for bitfield

--- bitfield/bitfield_test.go
+++ bitfield/bitfield_test.go
@@ -5,31 +5,96 @@ import (
 )
 
 const bitfieldLength = 896
-const bitfieldArrayLength = 4
 
-func TestBitfieldCreation(t *testing.T) {
-
-	const testIndex = 764
+func TestBitfield_Set(t *testing.T) {
 
 	b := NewBitfield(bitfieldLength)
 
-	if b.arrayLength != 112 {
+	if b.arrayLength != bitfieldLength/8 {
 		t.Error("Bitfield byte array has wrong length ")
 	}
 
-	if b.Get(testIndex) != 0 {
-		t.Error("Value at", testIndex, "is not zero after init")
+	b.Set(0)
+	if b.field[0] != 0x80 {
+		t.Error()
 	}
 
-	b.Set(testIndex)
-	if b.Get(testIndex) != 1 {
-		t.Error("Value at", testIndex, "is not one after set")
+	b.Set(5)
+	if b.field[0] != 0x84 {
+		t.Error()
 	}
 
-	b.Clear(testIndex)
-	if b.Get(testIndex) != 0 {
-		t.Error("Value at", testIndex, "is not zero after clear")
-
+	b.Set(7)
+	if b.field[0] != 0x85 {
+		t.Error()
 	}
 
+	b.Set(8)
+	if b.field[1] != 0x80 {
+		t.Error()
+	}
+
+	b.Set(125)
+	if b.field[15] != 0x04 {
+		t.Error()
+	}
+
+}
+
+func TestBitfield_Clear(t *testing.T) {
+
+	b := NewBitfield(bitfieldLength)
+
+	if b.arrayLength != bitfieldLength/8 {
+		t.Error("Bitfield byte array has wrong length ")
+	}
+
+	b.field[0] = 0x85
+	b.Clear(7)
+	if b.field[0] != 0x84 {
+		t.Error()
+	}
+
+	b.Clear(5)
+	if b.field[0] != 0x80 {
+		t.Error()
+	}
+
+	b.Clear(0)
+	if b.field[0] != 0x00 {
+		t.Error()
+	}
+
+	b.field[15] = 0x04
+	b.Clear(125)
+	if b.field[15] != 0x00 {
+		t.Error()
+	}
+
+}
+
+func TestAnd(t *testing.T) {
+
+	b := NewBitfield(bitfieldLength)
+	a := NewBitfield(bitfieldLength)
+
+	a.field[15] = 0x85
+	b.field[15] = 0x86
+
+	c := And(a, b)
+
+	if c.field[15] != 0x85&0x86 {
+		t.Error()
+	}
+
+}
+
+func BenchmarkAnd(b *testing.B) {
+
+	d := NewBitfield(1024 * 1024)
+	a := NewBitfield(1024 * 1024)
+
+	for i := 0; i < b.N; i++ {
+		_ = And(a, d)
+	}
 }
