a1085d6e9b68d38c3683f3f7c01de20dfc53aa51
Yuriy Lezhenin <yuriy.lezhenin@spbpu.com>
2019-03-18 12:45:54 +0300

    add logical ops to bitfield

--- bitfield/bitfield.go
+++ bitfield/bitfield.go
@@ -9,59 +9,37 @@ type Bitfield struct {
 	arrayLength     uint
 }
 
-func NewBitfield(length uint) (b *Bitfield) {
+func (b *Bitfield) convertIndex(index uint) (byteIndex, bitIndex uint) {
 
-	b = new(Bitfield)
-
-	b.effectiveLength = length
-	b.arrayLength = length / 8
-	if length%8 > 0 {
-		b.arrayLength += 1
-	}
-	b.length = b.arrayLength * 8
-
-	b.field = make([]byte, b.arrayLength)
-
-	return b
-}
-
-func NewBitfieldFromBytes(field []byte, effectiveLength uint) (b *Bitfield, err error) {
-
-	if len(field)*8 < int(effectiveLength) {
-		return nil,
-			fmt.Errorf("new butField: byte slice len less than field len")
+	if index > b.effectiveLength {
+		panic(fmt.Errorf("convert index: index (%d) > effective length (%d)",
+			index, b.effectiveLength))
 	}
 
-	b = new(Bitfield)
+	byteIndex = index / 8
+	bitIndex = index % 8
 
-	b.effectiveLength = effectiveLength
-	b.arrayLength = uint(len(field))
-	b.length = b.arrayLength * 8
-
-	b.field = make([]byte, len(field))
-	copy(b.field, field)
-
-	return b, nil
+	return byteIndex, bitIndex
 }
 
 func (b *Bitfield) Set(index uint) {
 
 	byteIndex, bitIndex := b.convertIndex(index)
-	b.field[byteIndex] = b.field[byteIndex] | (0x01 << bitIndex)
+	b.field[byteIndex] = b.field[byteIndex] | (0x80 >> bitIndex)
 }
 
 func (b *Bitfield) Clear(index uint) {
 
 	byteIndex, bitIndex := b.convertIndex(index)
-	b.field[byteIndex] = b.field[byteIndex] &^ (0x01 << bitIndex)
+	b.field[byteIndex] = b.field[byteIndex] &^ (0x80 >> bitIndex)
 }
 
 func (b *Bitfield) Get(index uint) (val byte) {
 
 	byteIndex, bitIndex := b.convertIndex(index)
-	val = (b.field[byteIndex] >> bitIndex) & 0x01
+	val = (b.field[byteIndex] << bitIndex) & 0x80
 
-	return val
+	return val >> 7
 }
 
 func (b *Bitfield) Bytes() (val []byte) {
@@ -85,10 +63,98 @@ func (b *Bitfield) GetIndices(valueAt byte) (indices []uint) {
 	return indices
 }
 
-func (b *Bitfield) convertIndex(index uint) (byteIndex, bitIndex uint) {
+func NewBitfield(length uint) (b *Bitfield) {
 
-	byteIndex = index / 8
-	bitIndex = 8 - (index % 8) - 1
+	b = new(Bitfield)
 
-	return byteIndex, bitIndex
+	b.effectiveLength = length
+	b.arrayLength = length / 8
+	if length%8 > 0 {
+		b.arrayLength += 1
+	}
+	b.length = b.arrayLength * 8
+
+	b.field = make([]byte, b.arrayLength)
+
+	return b
+}
+
+func NewBitfieldFromBytes(field []byte, effectiveLength uint) (b *Bitfield, err error) {
+
+	if len(field)*8 < int(effectiveLength) {
+		return nil,
+			fmt.Errorf("new bitfield: byte slice len less than field len")
+	}
+
+	b = new(Bitfield)
+
+	b.effectiveLength = effectiveLength
+	b.arrayLength = uint(len(field))
+	b.length = b.arrayLength * 8
+
+	b.field = make([]byte, len(field))
+	copy(b.field, field)
+
+	return b, nil
+}
+
+func Xor(a *Bitfield, b *Bitfield) (c *Bitfield) {
+
+	checkLength(a, b)
+
+	c = NewBitfield(a.effectiveLength)
+
+	for i := uint(0); i < a.arrayLength; i++ {
+		c.field[i] = a.field[i] ^ b.field[i]
+	}
+
+	return c
+}
+
+func And(a *Bitfield, b *Bitfield) (c *Bitfield) {
+
+	checkLength(a, b)
+
+	c = NewBitfield(a.effectiveLength)
+
+	for i := uint(0); i < a.arrayLength; i++ {
+		c.field[i] = a.field[i] & b.field[i]
+	}
+
+	return c
+}
+
+func Or(a *Bitfield, b *Bitfield) (c *Bitfield) {
+
+	checkLength(a, b)
+
+	c = NewBitfield(a.effectiveLength)
+
+	for i := uint(0); i < a.arrayLength; i++ {
+		c.field[i] = a.field[i] | b.field[i]
+	}
+
+	return c
+}
+
+func AndNot(a *Bitfield, b *Bitfield) (c *Bitfield) {
+
+	checkLength(a, b)
+
+	c = NewBitfield(a.effectiveLength)
+
+	for i := uint(0); i < a.arrayLength; i++ {
+		c.field[i] = a.field[i] &^ b.field[i]
+	}
+
+	return c
+}
+
+func checkLength(a *Bitfield, b *Bitfield) {
+	if a.effectiveLength != b.effectiveLength {
+		panic("xor: bitfields have different length")
+	}
+	if a.arrayLength != b.arrayLength {
+		panic("xor: underlying arrays have different length")
+	}
 }
