088f11ae30090d597cde54b5dea9fa5904b5b691
Yuriy Lezhenin <yuriy.lezhenin@spbpu.com>
2019-04-16 21:42:37 +0300

    split gui and cli test

--- main/gtkgui.go
+++ main/gtkgui.go
@@ -1,1 +1,17 @@
 package main
+
+import (
+	"github.com/lezhenin/gotorrentclient/gui"
+	"os"
+)
+
+func main() {
+
+	gTorrent, err := gui.NewGTorrent()
+	if err != nil {
+		panic(err)
+	}
+
+	os.Exit(gTorrent.Run())
+
+}
---
+++ test/testcli.go
@@ -0,0 +1,45 @@
+package test
+
+import (
+	"fmt"
+	"github.com/lezhenin/gotorrentclient/torrent"
+	"os"
+	"sync"
+)
+
+func main() {
+
+	args := os.Args
+
+	if len(args) < 3 {
+		panic(args)
+	}
+
+	fmt.Println(args)
+
+	torrentFile := os.Args[1]
+	folder := os.Args[2]
+
+	metadata, err := torrent.NewMetadata(torrentFile)
+	if err != nil {
+		panic(err)
+	}
+
+	download, err := torrent.NewDownload(metadata, folder)
+
+	var wait sync.WaitGroup
+	wait.Add(1)
+
+	go func() {
+		defer wait.Done()
+		download.Start()
+	}()
+
+	<-download.Done
+	download.Stop()
+
+	wait.Wait()
+
+	fmt.Println("Done")
+
+}
