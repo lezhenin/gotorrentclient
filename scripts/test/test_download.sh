f65ac51e222835fd11e69edc94af1641a556596c
Yuriy Lezhenin <yuriy.lezhenin@spbpu.com>
2019-04-18 18:34:01 +0300

    add script for integration test

---
+++ scripts/test/test_download.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+TIMOUT_SECS=30
+TEST_RETURN_CODE=0
+TIMOUT_RETURN_CODE=0
+
+echo "create docker network"
+docker network create net-torrent-test
+
+echo "run docker containers"
+docker run --network=net-torrent-test -itd --name=test-tracker webtorrent-tracker
+docker run --network=net-torrent-test -itd --name=test-seeder --link test-tracker:tracker  webtorrent-client
+docker run --network=net-torrent-test -itd --name=test-leecher --link test-tracker:tracker --link test-original-seeder:seeder gotorrent-client
+
+# run seeder
+echo "run seeding client"
+docker exec -d test-seeder \
+webtorrent download ./download/test_data_docker.torrent -o ./download/ --keep-seeding -q
+
+# wait announce
+sleep 5
+
+echo "run leeching client"
+timeout --signal=SIGINT ${TIMOUT_SECS} \
+docker exec test-leecher \
+gotorrentcli -t ./download/test_data_docker.torrent -o ./output/ -v 4
+
+TIMOUT_RETURN_CODE=$?
+if [[ ${TIMOUT_RETURN_CODE} -ne 0 ]]; then
+    echo "time is out: ${TIMOUT_SECS}"
+    TEST_RETURN_CODE=1
+fi
+
+echo "stop docker containers"
+docker stop test-leecher
+docker stop test-seeder
+docker stop test-tracker
+
+echo "remove docker containers"
+docker rm test-leecher
+docker rm test-seeder
+docker rm test-tracker
+
+echo "remove docker network"
+docker network rm net-torrent-test
+
+exit ${TEST_RETURN_CODE}
