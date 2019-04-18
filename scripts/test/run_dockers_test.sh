e0edadcb5c462b45733020f98218e0e1ad4862b5
Yuriy Lezhenin <yuriy.lezhenin@spbpu.com>
2019-04-18 16:44:56 +0300

    add echos to test script; add log folder

--- scripts/test/run_dockers_test.sh
+++ scripts/test/run_dockers_test.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
-TIMOUT_SECS=10
+TIMOUT_SECS=30
 TEST_RETURN_CODE=0
 TIMOUT_RETURN_CODE=0
 
+echo "create docker network"
 docker network create net-torrent-test
-docker run --network=net-torrent-test -itd --name=test-tracker webtorrent-tracker
+
+echo "run docker containers"
+docker run --network=net-torrent-test -itd --name=test-tracker -p 8000:8000 webtorrent-tracker
 docker run --network=net-torrent-test -itd --name=test-original-seeder --link test-tracker:tracker  webtorrent-client
 docker run --network=net-torrent-test -itd --name=test-client-1 --link test-tracker:tracker --link test-original-seeder:seeder gotorrent-client
 docker run --network=net-torrent-test -itd --name=test-client-2 --link test-tracker:tracker --link test-original-seeder:seeder webtorrent-client
 
 # run seeder
+echo "run client 1"
 docker exec -d test-original-seeder \
 webtorrent download ./download/test_data_docker.torrent -o ./download/ --keep-seeding -q
 
@@ -18,15 +22,21 @@ webtorrent download ./download/test_data_docker.torrent -o ./download/ --keep-se
 sleep 5
 
 # run downloading
-docker exec -d test-client-1 \
-gotorrentcli -t ./download/test_data_docker.torrent -o ./output/ -s
+echo "run client 2"
+docker exec -i test-client-1 \
+gotorrentcli -t ./download/test_data_docker.torrent -o ./output/ -s -v 4 &
+
+# wait announce
+sleep 5
 
 # run downloading with blocked seeder
+
 docker exec test-client-2 \
 bash -c "getent hosts seeder | awk '{ print \$1 }' > blocklist.txt"
 
+echo "run client 3"
 timeout --signal=SIGINT ${TIMOUT_SECS} \
-docker exec -i test-client-2 \
+docker exec test-client-2 \
 webtorrent download ./download/test_data_docker.torrent -o ./output/ -b ./blocklist.txt
 
 TIMOUT_RETURN_CODE=$?
@@ -35,17 +45,19 @@ if [[ ${TIMOUT_RETURN_CODE} -ne 0 ]]; then
     TEST_RETURN_CODE=1
 fi
 
+echo "stop docker containers"
 docker stop test-client-2
 docker stop test-client-1
 docker stop test-original-seeder
 docker stop test-tracker
 
+echo "remove docker containers"
 docker rm test-client-2
 docker rm test-client-1
 docker rm test-original-seeder
 docker rm test-tracker
 
+echo "remove docker network"
 docker network rm net-torrent-test
-
 
 exit ${TEST_RETURN_CODE}
